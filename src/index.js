fc97913e3e56ffa5cdcd48bdf4447b465b35434f
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-02-27 21:26:02 +0000

    created store and experimented with some redux features

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { configureStore, createAction, createReducer } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux';
+
+const initialState = { state: 0 }
+const reducer = createReducer(initialState, (builder) => {
+  builder
+    .addDefaultCase((state, action) => {
+      return state
+    })
+})
+
+const store = configureStore({ reducer: rootReducer })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
