2d6f41db2d1fd86f634c115c37843e77a4d968b3
unknown <brandonalexescu@yahoo.com>
2023-03-02 17:12:49 +0000

    started work on 'whos watching?' text

---
+++ src/components/WhosWatchingContainer/WhosWatchingContainer.js
@@ -0,0 +1,11 @@
+import './WhosWatchingContainer.css'
+
+const WhosWatchingContainer = () => {
+    return (
+        <div className="whosWatchingContainer row justify-content-center w-100 bg-warning position-relative">
+
+        </div>
+    )
+}
+
+export default WhosWatchingContainer;
