57ae3cac2d647d9ac7870c1010485dd7ffef8cb5
unknown <brandonalexescu@yahoo.com>
2023-03-08 07:47:36 +0000

    created container for profile icons

---
+++ src/components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer.js
@@ -0,0 +1,9 @@
+import './ProfileIconsContainer.css';
+
+const ProfileIconsContainer = () => {
+    return (
+        <div className='profileIconsContainer row justify-content-center align-items-center w-100 position-relative'></div>
+    )
+}
+
+export default ProfileIconsContainer;
