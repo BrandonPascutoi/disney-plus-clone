55b47be16cb1589a03964fc80c408b49b1a11d47
unknown <brandonalexescu@yahoo.com>
2023-03-08 07:48:03 +0000

    imported and added profilePickerSection to layout

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import EditProfilesButtonContainer from './components/EditProfilesButtonContainer/EditProfilesButtonContainer';
 import WhosWatchingContainer from './components/WhosWatchingContainer/WhosWatchingContainer'
+import ProfileIconsContainer from './components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer';
 
 function App() {
   return (
     <div className="App h-100 d-flex flex-column align-items-center container-fluid justify-content-start">
       <EditProfilesButtonContainer />
       <WhosWatchingContainer />
+      <ProfileIconsContainer />
     </div>
   );
 }
