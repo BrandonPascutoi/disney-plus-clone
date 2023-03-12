77ae3c7962358f6d32c89c8c65501962fdd1157a
Brandon Pascutoi <brandonalexescu@yahoo.com>
2023-03-12 21:25:38 +0000

    changed how this container spaces child elements

--- src/components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer.js
+++ src/components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer.js
@@ -1,8 +1,12 @@
 import './ProfileIconsContainer.css';
+import IndividualProfileContainer from './IndividualProfileContainer/IndividualProfileContainer';
 
 const ProfileIconsContainer = () => {
     return (
-        <div className='profileIconsContainer row justify-content-center align-items-center w-100 position-relative'></div>
+        <div className='profileIconsContainer row align-items-center w-100 position-relative'>
+            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/dr-strange.png'}/>
+            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/sarah.png'}/>
+        </div>
     )
 }
 
