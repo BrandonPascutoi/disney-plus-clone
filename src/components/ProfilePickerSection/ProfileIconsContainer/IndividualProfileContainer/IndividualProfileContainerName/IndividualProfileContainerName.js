72525a3db06736cda44a707b2edf977c76b589c1
unknown <brandonalexescu@yahoo.com>
2023-03-14 17:06:41 +0000

    created component for user profile names

---
+++ src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainerName/IndividualProfileContainerName.js
@@ -0,0 +1,9 @@
+import './IndividualProfileContainerName.css';
+
+const IndividualProfileContainerName = props => {
+    return (
+        <h1 className='profileName text-white'>{props.name}</h1>
+    )
+}
+
+export default IndividualProfileContainerName;
