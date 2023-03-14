4d08c625431788fb0cc440b555d0839e46762526
unknown <brandonalexescu@yahoo.com>
2023-03-14 17:10:25 +0000

    added name prop to components to render profile name

--- src/components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer.js
+++ src/components/ProfilePickerSection/ProfileIconsContainer/ProfileIconsContainer.js
@@ -4,8 +4,8 @@ import IndividualProfileContainer from './IndividualProfileContainer/IndividualP
 const ProfileIconsContainer = () => {
     return (
         <div className='profileIconsContainer row align-items-center w-100 position-relative'>
-            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/dr-strange.png'}/>
-            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/sarah.png'}/>
+            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/dr-strange.png'} name='Brandon'/>
+            <IndividualProfileContainer image={process.env.PUBLIC_URL + '/images/sarah.png'} name='Irina'/>
         </div>
     )
 }
