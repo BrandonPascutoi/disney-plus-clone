a50cd3269c98ab759f551028aa4bff7a8f819aaf
unknown <brandonalexescu@yahoo.com>
2023-03-14 17:13:20 +0000

    imported name component and implemented it into parent component. also added flex styling to individualProfileContainer

--- src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainer.js
+++ src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainer.js
@@ -1,10 +1,12 @@
 import './IndividualProfileContainer.css';
 import IndividualProfileContainerImage from './IndividualProfileContainerImage/IndividualProfileContainerImage';
+import IndividualProfileContainerName from './IndividualProfileContainerName/IndividualProfileContainerName';
 
 const IndividualProfileContainer = props => {
     return (
-        <div className='individualProfileContainer h-100 w-25'>
+        <div className='individualProfileContainer h-100 w-25 d-flex flex-column justify-content-between align-items-center'>
             <IndividualProfileContainerImage image={props.image}/>
+            <IndividualProfileContainerName name={props.name}/>
         </div>
     )
 }
