d4e1c06311a8f9e6f8d6400aa15a751b1dc894bb
unknown <brandonalexescu@yahoo.com>
2023-03-15 06:47:14 +0000

    imported IndividualProfileContainerLock component

--- src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainer.js
+++ src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainer.js
@@ -1,12 +1,14 @@
 import './IndividualProfileContainer.css';
 import IndividualProfileContainerImage from './IndividualProfileContainerImage/IndividualProfileContainerImage';
 import IndividualProfileContainerName from './IndividualProfileContainerName/IndividualProfileContainerName';
+import IndividualProfileContainerLock from './IndividualProfileContainerLock/IndividualProfileContainerLock';
 
 const IndividualProfileContainer = props => {
     return (
         <div className='individualProfileContainer h-100 w-25 d-flex flex-column justify-content-between align-items-center'>
             <IndividualProfileContainerImage image={props.image}/>
             <IndividualProfileContainerName name={props.name}/>
+            <IndividualProfileContainerLock />
         </div>
     )
 }
