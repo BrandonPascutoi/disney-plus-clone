7541c04238650c0d6bb3d55a8fd6d45e0f02fa41
unknown <brandonalexescu@yahoo.com>
2023-03-15 06:48:15 +0000

    created lock logo component for user profiles

---
+++ src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainerLock/IndividualProfileContainerLock.js
@@ -0,0 +1,9 @@
+import './IndividualProfileContainerLock.css';
+
+const IndividualProfileContainerLock = () => {
+    return (
+        <img src={process.env.PUBLIC_URL + '/images/lock.png'}/>
+    )
+}
+
+export default IndividualProfileContainerLock;
