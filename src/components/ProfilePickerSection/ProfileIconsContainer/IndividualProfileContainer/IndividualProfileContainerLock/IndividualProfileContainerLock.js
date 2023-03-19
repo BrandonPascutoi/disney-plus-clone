eaabbd6e01341789133fd378a036a88ff315555c
unknown <brandonalexescu@yahoo.com>
2023-03-19 20:26:39 +0000

    created lock image component that appears depending on wether or not profile is locked

--- src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainerLock/IndividualProfileContainerLock.js
+++ src/components/ProfilePickerSection/ProfileIconsContainer/IndividualProfileContainer/IndividualProfileContainerLock/IndividualProfileContainerLock.js
@@ -1,9 +1,15 @@
 import './IndividualProfileContainerLock.css';
 
-const IndividualProfileContainerLock = () => {
-    return (
-        <img src={process.env.PUBLIC_URL + '/images/lock.png'}/>
-    )
+const IndividualProfileContainerLock = props => {
+    if (props.locked === true) {
+        return (
+            <img src={process.env.PUBLIC_URL + '/images/lock.png'}/>
+        )
+    } else if (props.locked === false) {
+        return (
+            <img style={{opacity: 0}} src={process.env.PUBLIC_URL + '/images/lock.png'}/>
+        )
+    }
 }
 
 export default IndividualProfileContainerLock;
